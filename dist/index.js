532b3e0467175f56f85bd8a970bea307680e293b
Mussa Ali Haider <musaalihaider5@gmail.com>
2024-07-30 12:32:28 +0500

    word counter

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,12 @@
+#! /usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//import { input } from "@inquirer/prompts";
-//async function sentences() {
-//  const paragraph: string = await input({
-//   message: "Welcome, Type a paragraph and see how many words are there",
-//  required: true,
-// });
-// const numofwords: string = paragraph.trim().split(" ");
-// console.log(numofwords);
-// console.log(`Number of words = ${numofwords.length}`);
-//}
-//sentences();
 const prompts_1 = require("@inquirer/prompts");
 async function sentences() {
     const paragraph = await (0, prompts_1.input)({
         message: "Welcome, Type a paragraph and see how many words are there",
         required: true,
     });
-    // Trim the paragraph and split it by spaces, then filter out any empty strings
     const words = paragraph
         .trim()
         .split(/\s+/)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import { input } from "@inquirer/prompts";
 
 async function sentences() {
